efa631764a0c545184891e15018e8806a5cfe0a1
othmanefc <othmane.hassani1@gmail.com>
2024-08-11 18:49:02 +0200

    equality operators

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,6 +1,14 @@
+// use convert_case::{Case, Casing};
+// use std::fmt;
 use std::io::{self, Write};
 use std::process;
 use strum_macros::Display;
+
+#[derive(Display)]
+#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
+enum Operator {
+    EqualEqual,
+}
 
 #[derive(Display)]
 #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
@@ -15,9 +23,35 @@ enum TokenType {
     Plus,
     Minus,
     Semicolon,
+    Equal,
+    #[strum(to_string = "{op}")]
+    Operator {
+        op: Operator,
+    },
     EOF,
     Unknown(String),
 }
+
+// impl fmt::Display for TokenType {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self {
+//             TokenType::Operator(op) => write!(f, "{}", op),
+//             TokenType::Unknown(unknown_str) => write!(f, "UNKNOWN({})", unknown_str),
+//             _ => panic!(""),
+//         }
+//     }
+// }
+// impl fmt::Display for TokenType {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self {
+//             TokenType::Operator(op) => {
+//                 write!(f, "{}", op.to_string().to_case(Case::ScreamingSnake))
+//             }
+//             TokenType::Unknown(s) => write!(f, "UNKNOWN({})", s),
+//             _ => write!(f, "{}", &self.to_string().to_case(Case::ScreamingSnake)),
+//         }
+//     }
+// }
 
 struct Token {
     token_type: TokenType,
@@ -25,10 +59,25 @@ struct Token {
     line: usize,
 }
 
+fn operators(
+    char_: Option<char>,
+    operator_type: TokenType,
+    simple_type: TokenType,
+    match_char: char,
+) -> TokenType {
+    if let Some(i) = char_ {
+        if i == match_char {
+            return operator_type;
+        }
+    }
+    return simple_type;
+}
+
 fn tokenize(line: &str, line_number: usize) -> Vec<Token> {
     let mut tokens = Vec::<Token>::new();
+    let mut chars = line.chars().peekable();
 
-    for char in line.chars() {
+    while let Some(char) = chars.next() {
         let token_type = match char {
             '(' => TokenType::LeftParen,
             ')' => TokenType::RightParen,
@@ -40,11 +89,26 @@ fn tokenize(line: &str, line_number: usize) -> Vec<Token> {
             '+' => TokenType::Plus,
             ';' => TokenType::Semicolon,
             '-' => TokenType::Minus,
+            '=' => operators(
+                chars.peek().copied(),
+                TokenType::Operator {
+                    op: Operator::EqualEqual,
+                },
+                TokenType::Equal,
+                '=',
+            ),
             _ => TokenType::Unknown(char.into()),
         };
         tokens.push(Token {
             lexeme: match token_type {
                 TokenType::Unknown(_) => String::new(),
+                TokenType::Operator { op: _ } => {
+                    let mut s = char.to_string();
+                    if let Some(next_char) = chars.next() {
+                        s.push(next_char)
+                    }
+                    s
+                }
                 _ => char.into(),
             },
             token_type,
