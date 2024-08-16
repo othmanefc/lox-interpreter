78d005ac7eccb0ae76aecc157bc3f92ae87e9a01
othmanefc <othmane.hassani@pricehubble.com>
2024-08-16 12:36:51 +0200

    evaluate paren

--- src/evaluate/evaluate.rs
+++ src/evaluate/evaluate.rs
@@ -8,6 +8,28 @@ fn evaluate_expr(expr: &Expr) -> Result<Box<dyn Display>, &'static str> {
         Expr::String(s) => Ok(Box::new(s.to_owned())),
         Expr::Bool(b) => Ok(Box::new(b.to_owned())),
         Expr::Nil => Ok(Box::new("nil")),
+        // Expr::Grouping(vec_expr) => {
+        // let mut final_ev = Vec::new();
+        // for ex in vec_expr {
+        //     let ev = evaluate_expr(ex)?;
+        //     final_ev.push(ev)
+        // }
+        // Ok(Box::new(final_ev))
+        // let evaluated: Result<Vec<Box<dyn Display>>, &'static str> =
+        //     vec_expr.iter().map(evaluate_expr).collect();
+        // Ok(Box::new(evaluated?))
+        // }
+        Expr::Grouping(vec_expr) => {
+            let evaluated: Result<Vec<Box<dyn Display>>, &'static str> =
+                vec_expr.iter().map(evaluate_expr).collect();
+            let result = evaluated?
+                .iter()
+                .map(|e| format!("{}", e))
+                .collect::<Vec<String>>()
+                .join(", ");
+
+            Ok(Box::new(result))
+        }
         _ => Err("Unsupported expression type"),
     }
 }
